df63ae87e7517f2b8f9628c6df44fcdc852e4912
dinkovs <dinkovs@ufl.edu>
2014-02-27 12:41:08 -0500

    Screen Shell

---
+++ deep-blue/src/com/me/deepblue/GameScreen.java
@@ -0,0 +1,55 @@
+package com.me.deepblue;
+
+import com.badlogic.gdx.Screen;
+
+public class GameScreen implements Screen{
+
+	private DeepBlue game;
+	
+	public GameScreen(DeepBlue game){
+		this.game = game;
+	}
+	
+	@Override
+	public void render(float delta) {
+		
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		
+		
+	}
+
+	@Override
+	public void show() {
+		
+		
+	}
+
+	@Override
+	public void hide() {
+		
+		
+	}
+
+	@Override
+	public void pause() {
+		
+		
+	}
+
+	@Override
+	public void resume() {
+		
+		
+	}
+
+	@Override
+	public void dispose() {
+		
+		
+	}
+
+}
