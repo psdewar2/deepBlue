04a3685b2623d4c272c7dc2a95c46c6af5d6ec69
psdewar2 <psdewar2@live.com>
2014-03-13 19:31:00 -0400

    background music

--- deep-blue/src/com/me/deepblue/DeepBlue.java
+++ deep-blue/src/com/me/deepblue/DeepBlue.java
@@ -1,10 +1,12 @@
 package com.me.deepblue;
 
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Game;
-import com.me.screens.GameScreen;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.me.screens.MenuScreen;
 
-public class DeepBlue extends Game{
+public class DeepBlue extends Game implements ApplicationListener{
 
 	public MenuScreen main_menu_screen;
 	
@@ -13,6 +15,13 @@ public class DeepBlue extends Game{
 		main_menu_screen = new MenuScreen(this);
 		
 		setScreen(main_menu_screen);
+		
+		//background main menu music
+		Music mainMusic;
+		mainMusic = Gdx.audio.newMusic(Gdx.files.internal("menu/mainMusic.mp3"));
+		mainMusic.setLooping(true);
+		mainMusic.play();
+
+		}
 	}
 
-}
